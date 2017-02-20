dde285a53d8535c861ea9be1412a8cf4840253a9
david <david.tateyama@become.co.jp>
2017-02-20 18:29:40 +0900

    Add TweetReachComputer, Storage, and UserFollowersCounter actors.

--- app/actors/StatisticsProvider.scala
+++ app/actors/StatisticsProvider.scala
@@ -1,18 +1,41 @@
 package actors
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 
 /**
   * Created by david on 17/02/17.
   */
 class StatisticsProvider extends Actor with ActorLogging {
 
+  var reachComputer: ActorRef = _
+  var storage: ActorRef = _
+  var followersCounter: ActorRef = _
+
   override def preStart(): Unit = {
-    log.info("Hello, world.")
+    log.info("Starting StatisticsProvider")
+    followersCounter = context.actorOf(
+      UserFollowersCounter.props,
+      name = "userFollowersCounter"
+    )
+    storage = context.actorOf(
+      Storage.props,
+      name = "storage"
+    )
+    reachComputer = context.actorOf(
+      TweetReachComputer.props(followersCounter, storage),
+      name = "tweetReachComputer"
+    )
   }
 
   def receive = {
     case message => // do nothing
+  }
+
+  override def unhandled(message: Any): Unit = {
+    log.warning(
+      "Unhandled message {} message from {}", message, sender()
+    )
+    super.unhandled(message)
   }
 }
 
