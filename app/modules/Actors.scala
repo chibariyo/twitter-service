e33ba939b5998c9871436565d0e107d19a581435
david <david.tateyama@become.co.jp>
2017-02-20 18:20:26 +0900

    Reformat.

--- app/modules/Actors.scala
+++ app/modules/Actors.scala
@@ -9,7 +9,10 @@ import com.google.inject.AbstractModule
   * Created by david on 17/02/17.
   */
 class Actors @Inject()(system: ActorSystem) extends ApplicationActors {
-  system.actorOf(props = StatisticsProvider.props, name = "statisticsProvider")
+  system.actorOf(
+    props = StatisticsProvider.props,
+    name = "statisticsProvider"
+  )
 }
 
 trait ApplicationActors
