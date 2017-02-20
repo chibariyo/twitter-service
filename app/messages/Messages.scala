96c05ad5063d230df56f68308474583a89ce03a1
david <david.tateyama@become.co.jp>
2017-02-20 19:55:55 +0900

    fetch userId as BigInt.

--- app/messages/Messages.scala
+++ app/messages/Messages.scala
@@ -6,8 +6,8 @@ package messages
 case class ComputeReach(tweetId: BigInt)
 case class TweetReach(tweetId: BigInt, score: Int)
 
-case class FetchFollowerCount(user: String)
-case class FollowerCount(user: String, followersCount: Int)
+case class FetchFollowerCount(tweetId: BigInt, userId: BigInt)
+case class FollowerCount(tweetId: BigInt, userId: BigInt, followersCount: Int)
 
 case class StoreReach(tweetId: BigInt, score: Int)
 case class ReachStored(tweetId: BigInt)
