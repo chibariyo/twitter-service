e3f71e0645a7847d29d20cdaeff3b1a748c6aada
david <david.tateyama@become.co.jp>
2017-02-20 21:26:21 +0900

    Define a custom supervisor strategy to deal with different exceptions.

--- app/actors/StatisticsProvider.scala
+++ app/actors/StatisticsProvider.scala
@@ -1,6 +1,9 @@
 package actors
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.actor.SupervisorStrategy.{Escalate, Restart}
+import akka.actor.{Actor, ActorLogging, ActorRef, OneForOneStrategy, Props, SupervisorStrategy}
+import reactivemongo.core.errors.ConnectionException
+import scala.concurrent.duration._
 
 /**
   * Created by david on 17/02/17.
@@ -26,6 +29,14 @@ class StatisticsProvider extends Actor with ActorLogging {
       name = "tweetReachComputer"
     )
   }
+
+  override def supervisorStrategy: SupervisorStrategy =
+    OneForOneStrategy(maxNrOfRetries = 3, withinTimeRange = 2.minutes) {
+      case _: ConnectionException =>
+        Restart
+      case t: Throwable =>
+        super.supervisorStrategy.decider.applyOrElse(t, (_: Any) => Escalate)
+    }
 
   def receive = {
     case message => // do nothing
